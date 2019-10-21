75462e93f4b3abcf33779bfbd7062517c62f0826
Victor Souza <victor.souza@educare.digital>
2019-10-21 12:42:34 -0300

    Feature: adicionado cenário do jogo e ajustados parametros de movimentacao do jogador.

---
+++ Assets/Scripts/MESHTEST.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MESHTEST : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+     {
+        Mesh m = GetComponent<MeshFilter>().mesh;
+        m.bounds = new Bounds(Vector3.zero, Vector3.one * 2000);
+         //mesh.bounds is in local/object space, so
+         //setting a center of zero and extents of 2000 will
+         //expand the bounds into a box 2000 units wide around
+         //the center of the object
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
