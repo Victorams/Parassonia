0aaf164f92c98f3ed216db326a1571078712230c
Tio Peter <pedrohenrique9702@gmail.com>
2020-12-08 13:49:48 -0300

    Crosshair e Inicio de logica de lampadas

---
+++ Assets/Scripts/LampScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+	Light lightComp;
+	[SerializeField] private GameObject LightSwitch;
+	[SerializeField] private Color color;
+	[SerializeField] private int range;
+	[SerializeField] private float intensity;
+	private bool switchState;
+	private LightSwitchScript lightSwitchScript;
+
+    void Start()
+    {
+    	lightComp = gameObject.AddComponent<Light>();
+        lightComp.color = color;
+        lightComp.range = range;	
+        lightComp.intensity = intensity;
+        lightComp.shadows = LightShadows.Hard;
+        
+        //var lightSwitchScript : Control = LightSwitch.GetComponent("Light Switch Script");
+ 		lightSwitchScript = LightSwitch.gameObject.GetComponent<LightSwitchScript>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switchState = lightSwitchScript.getSwitchState();
+
+        if(switchState == true){
+        	lightComp.intensity = intensity;
+        }else{
+        	lightComp.intensity = 0;
+        }
+    }
+
+}
---
+++ Assets/Scripts/LightSwitchScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitchScript : MonoBehaviour
+{
+	[SerializeField] private bool isOn;
+
+	public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null)
+            player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            toggleState();
+        }
+    }
+
+    private void toggleState(){
+    	if (isOn == true){
+    		isOn = false;
+    	}else{
+    		isOn = true;
+    	}
+    }
+
+    public bool getSwitchState(){
+    	return isOn;
+    }
+}
